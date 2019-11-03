9d94781c0d00f1d38778403c49ec2a5c57b45a15
Eugen Kuhn <eugenkuhn@eugens-imac.localdomain>
2019-11-03 16:15:15 +0100

    add fetch function and return some data

--- src/App.js
+++ src/App.js
@@ -4,23 +4,46 @@ import axios from "axios";
 
 class App extends React.Component {
 
-  componentDidMount(){
-      const getData = async () => {
-          try{
-              const response = axios.get("https://sidereumapi.netlify.com/");
-              console.log("response", response);
-          }
-          catch(error){
-             console.log(error)
-          }
-      }
+  state = {
+      data: null
+  }
 
-      getData();
+  componentDidMount(){
+
+    const getData = async () => {
+      try{
+        const response = await axios.get("https://sidereumapi2.herokuapp.com/saerge/getData");
+        console.log(response)
+        this.setState({data: response})
+        console.log(this.state.data)
+      }
+      catch(error){
+          console.log(error)
+      }
+    }
+    
+    getData();
+        
+           
   }
 
   render(){
+   
     return (
       <div>
+        {this.state.data  ?
+            this.state.data.data.map((elem, index) => {
+              return(
+                  <ul style={{marginLeft: "500px"}} key={index}>
+                      <li>{elem.titel}</li>
+                      <li>{elem.price}</li>
+                      <li>{elem.category}</li>
+                  </ul>
+              )
+          })
+            :   
+            <h1 style={{fontSize: "30px", marginLeft: "500px"}}>data is loading</h1>
+        }
        <Sidebar />
       </div>
     );
